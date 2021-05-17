eab7252c0e16eb78081fe4a677ea6f535de90bf7
yhcwawo <ghdcks1018@gmail.com>
2021-05-18 00:48:30 +0900

    first commit in wawo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { Navigation } from './components/navigation'
+import { Header } from './components/header'
+import { Features } from './components/features'
+import { About } from './components/about'
+import { Services } from './components/services'
+import { Gallery } from './components/gallery'
+import { Testimonials } from './components/testimonials'
+import { Team } from './components/Team'
+import { Contact } from './components/contact'
+import JsonData from './data/data.json'
+import SmoothScroll from 'smooth-scroll'
 
-function App() {
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData , setLandingPageData] = useState({});
+
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Header data={JsonData.Header} />
+      <Features data={JsonData.Features} />
+      <About data={JsonData.About} />
+      <Services data={JsonData.Services} />
+      <Gallery />
+      <Testimonials data={JsonData.Testimonials} />
+      <Team data={JsonData.Team} />
+      <Contact data={JsonData.Contact} /> 
     </div>
   );
-}
+};
 
-export default App;
+export default App
